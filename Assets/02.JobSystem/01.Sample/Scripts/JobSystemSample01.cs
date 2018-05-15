6a33ca6598c7d9b21a77ee7d7ef089c4e96d2e90
shigehiro-don <shigehiro.yoshiki@donuts.ne.jp>
2018-05-15 18:20:10 +0900

    JobSystemサンプルの追加

---
+++ Assets/02.JobSystem/01.Sample/Scripts/JobSystemSample01.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace Es.JobSystem.Sample._01
+{
+    public class JobSystemSample01 : MonoBehaviour
+    {
+        // Jobを作る際、Jobでアクセスされる全てのデータをJob内に宣言します。
+        struct VelocityJob : IJob
+        {
+            // 読み取り専用として宣言することにより複数のJobが並列にデータにアクセスできます。
+            [ReadOnly]
+            public NativeArray<Vector3> velocity;
+
+            // デフォルトでは、コンテナは読み書きが可能です。
+            public NativeArray<Vector3> position;
+
+            // Jobには一般的にフレームの概念がないため、deltaTimeをJobにコピーする必要があります。
+            // MainThreadは同じフレームまたは次のフレームでJobを待機しますが、Jobは
+            // WorkerThreadで独立して処理が実行されます。
+            public float deltaTime;
+
+            // Jobが実行するコードです。
+            public void Execute ()
+            {
+                for (var i = 0; i < position.Length; i++)
+                    position[i] = position[i] + velocity[i] * deltaTime;
+            }
+        }
+
+        public void Update ()
+        {
+            // NativeArrayはNativeContainer属性が付加されているので
+            // MainThreadとWorkerThreadで共有データとして扱われます(コピーされない)。
+            // また、使い終えたらDisposeする必要があります。
+            var position = new NativeArray<Vector3> (500, Allocator.Persistent);
+            var velocity = new NativeArray<Vector3> (500, Allocator.Persistent);
+            for (var i = 0; i < velocity.Length; i++)
+                velocity[i] = new Vector3 (0, 10, 0);
+
+            // Jobの初期化処理です。
+            var job = new VelocityJob ()
+            {
+                deltaTime = Time.deltaTime,
+                position = position,
+                velocity = velocity
+            };
+
+            // Jobをスケジューリングし、後でJobの完了を待つことができるJobHandleを返します。
+            JobHandle jobHandle = job.Schedule ();
+
+            // Jobが完了したことを確認します(完了してなければ完了まで待ちます)
+            // Schedule実行後、すぐにCompleteを呼び出すことはお勧めできません。
+            // 並列処理の恩恵を受けることがほぼできなくなるためです。
+            // フレームの早い段階でJobをScheduleし、他の処理を行った後でCompleteを呼び出すのが最適です
+            jobHandle.Complete ();
+
+            Debug.Log (job.position[0]);
+
+            position.Dispose ();
+            velocity.Dispose ();
+        }
+    }
+}
---
+++ Assets/02.JobSystem/02.Sample/Scripts/JobSystemSample02.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace Es.JobSystem.Sample._02
+{
+    public class JobSystemSample02 : MonoBehaviour
+    {
+        // Jobの並列化のためにIJobParallelForを実装するよう変更
+        struct VelocityJob : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray<Vector3> velocity;
+
+            public NativeArray<Vector3> position;
+
+            public float deltaTime;
+
+            // 並列アクセスのためにインデックスを受け取って処理を行うExecuteを実装
+            public void Execute (int i)
+            {
+                position[i] = position[i] + velocity[i] * deltaTime;
+            }
+        }
+
+        public void Update ()
+        {
+            var position = new NativeArray<Vector3> (500, Allocator.Persistent);
+
+            var velocity = new NativeArray<Vector3> (500, Allocator.Persistent);
+            for (var i = 0; i < velocity.Length; i++)
+                velocity[i] = new Vector3 (0, 10, 0);
+
+            var job = new VelocityJob ()
+            {
+                deltaTime = Time.deltaTime,
+                position = position,
+                velocity = velocity
+            };
+
+            // 並列実行のJobをスケジュールします。
+            // 最初のパラメータは、各反復が何回実行されるかです。
+            // 2番目のパラメータは、内部でのループ分割数(バッチ数)です。
+            JobHandle jobHandle = job.Schedule (position.Length, 1);
+
+            jobHandle.Complete ();
+
+            Debug.Log (job.position[0]);
+
+            position.Dispose ();
+            velocity.Dispose ();
+        }
+    }
+}
